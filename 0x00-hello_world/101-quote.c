22b8f95e0da276579872f31dfb6ae030fecaba20
John Atef <johnatef60@yahoo.com>
2022-12-10 13:57:59 -0800

    cout

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" << ;
+	return (1);
+}
